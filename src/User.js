26f767578dbe266f4565c3934d6c5a5dbb4eecf6
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-09 16:15:02 -0600

    Add search by name functionality

--- src/User.js
+++ src/User.js
@@ -10,24 +10,6 @@ class User {
     this.shoppinglist = [];
   }
 
-  // updateFavorites(recipe) {
-  //   if (!this.favRecipes.includes(recipe)) {
-  //     this.favRecipes.push(recipe)
-  //   } else {
-  //     let recipeIndex = this.favRecipes.indexOf(recipe);
-  //     this.favRecipes.splice(recipeIndex);
-  //   }
-  // }
-
-  // updateRecipesToCook(recipe) {
-  //   if (!this.recipesToCook.includes(recipe)) {
-  //     this.recipesToCook.push(recipe)
-  //   } else {
-  //     let recipeIndex = this.recipesToCook.indexOf(recipe);
-  //     this.recipesToCook.splice(recipeIndex);
-  //   }
-  // }
-
   updateSavedRecipes(savedList, recipe) {
     if (!savedList.includes(recipe)) {
       savedList.push(recipe)
@@ -37,43 +19,32 @@ class User {
     }
   }
 
-  // "Filter my favoriteRecipes or recipesToCook by type"
-  //***** should be okay if they're in different sections**** */
   filterSavedRecipes(savedList, tag) {
     return savedList.filter(recipe => {
       return recipe.tags.includes(tag);
-    }) 
+    }); 
   }
 
-  // filterFavRecipes(tag) {
-  //   return this.favRecipes.filter(recipe => {
-  //     return recipe.tags.includes(tag);
-  //   })
-  // }
-
-  // filterRecipesToCook(tag) {
-  //   return this.recipesToCook.filter(recipe => {
-  //     return recipe.tags.includes(tag);
-  //   })
-  // }
-
   searchSaved(searchItem) {
-  // this.favRecipes.filter(recipe => {
-  //     return recipe.name.includes(searchItem)    
     let ingredientId = null;
     ingredients.forEach((ingredient) => {
       if (searchItem.includes(ingredient.name)) {
         ingredientId = ingredient.id;
       }
-    })
+    });
     let searchList = [];
     this.favRecipes.forEach(recipe => {
       recipe.ingredients.forEach(item => {
         if (ingredientId === item.id) {
           searchList.push(recipe)
         }
-      })
-    })
+      });
+      this.favRecipes.filter(recipe => {
+        if (recipe.name.includes(searchItem)) {
+          searchList.push(recipe)
+        }
+      });
+    });
     return searchList;
   }
 }
