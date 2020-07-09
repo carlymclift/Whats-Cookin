73f6e839d9ec24ac6af5308f608250d6c8e1c989
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-09 16:20:31 -0600

    Add export links

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+
+
+
+const pickRandomUser = () => {
+  let randomUser = Math.floor((Math.random() * 49));
+  user = new User(usersData[randomUser].id, usersData[randomUser].name, usersData[randomUser].pantry)
+  const welcomeMessage = document.querySelector('.welcome-user');
+  welcomeMessage.innerHTML = `Welcome ${user.name}`;
+  return user;
+}
+
+const loadWindow = () => {
+  pickRandomUser()
+}
+
+window.onload = loadWindow()
