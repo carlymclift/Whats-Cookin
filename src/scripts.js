d5ee91adf7885aae3264502975a860dfdae7611e
Carly Clift <carlymclift@gmail.com>
2020-07-12 19:28:27 -0600

    Add buttons to the innerHTML of the populate cards function

--- src/scripts.js
+++ src/scripts.js
@@ -1,40 +1,47 @@
-// const usersData = require("../data/users");
+const recipeArea = document.querySelector('.card-container');
+const pantryArea = document.querySelector('.pantry');
+// const cardArea = document.querySelector('.card-container');
 
-// const ingredientsData = require("../data/ingredients");
-
-// const ingredientsData = require("../data/ingredients");
-
-const recipeArea = document.querySelector('.card-container')
-const pantryArea = document.querySelector('.pantry')
-
+// cardArea.addEventListener('click', filterCardConditions())
 let user;
 
 const pickRandomUser = () => {
   let randomUser = Math.floor((Math.random() * 49));
   user = new User(usersData[randomUser].id, usersData[randomUser].name, usersData[randomUser].pantry, )
   const welcomeMessage = document.querySelector('.welcome-user');
-  welcomeMessage.innerHTML = `Welcome ${user.name}!`;
+  welcomeMessage.innerHTML = `Welcome, ${user.name}!`;
   return user;
 }
 
 const populateCards = () => {
   recipeArea.innerHTML = ''
+
   recipeData.forEach(recipe => {
     recipeArea.innerHTML += `
     <section class='card-container'>
-    <div id='595736' class='fixed-recipe-card'>
-            <img class='recipe-image' src=${recipe.image} alt='Photo of Loaded Chocolate Chip Pudding Cookie Cups'>
-            <h3>${recipe.name}</h3>
-             <button id='save-recipe-button' class='save-recipe-button'>
-              <span class="save-recipe-button-text">Save recipe</span>
-              <img class='save-image' src='https://image.flaticon.com/icons/svg/942/942176.svg' alt='Save recipe button'>
-            </button>
-          </div>
-          </section>`
-  })
+      <div id='595736' class='fixed-recipe-card'>
+        <section class="card-buttons">
+          <button id='save-recipe-button' class='save-recipe-button'></button>
+          <button id='add-recipe-button' class='add-recipe-button'></button>
+        </section>
+        <h3>${recipe.name}</h3>
+        <input type="image" src=${recipe.image} name="recipe" class="recipe-image" id="recipe-image" />
+      </div>
+    </section>`
+})
 }
 
-  const translatePantry = (user) => {
+// const filterCardConditions = () => {
+//   if (event.target.classList.contains('save-recipe-button')) {
+//     addToFavorites();
+//   } else if (event.target.classList.contains('add-recipe-button')) {
+//     addMeal();
+//   } else if (event.target.classList.contains('recipe-image')) {
+//     displayDirections();
+//   }
+// }
+
+const translatePantry = (user) => {
   user.pantry.reduce((newPantry, item) => {
     ingredientsData.forEach(ingData => {
       let translatedPantry = {}
@@ -46,7 +53,6 @@ const populateCards = () => {
         newPantry.push(translatedPantry)
       }
     })
-    console.log('new pantry', newPantry);
     populatePantry(newPantry)
     return newPantry
   }, [])
@@ -68,5 +74,7 @@ const loadWindow = () => {
   translatePantry(user)
   // let recipe = new Recipe()
 }
+
+
 
 window.onload = loadWindow()
